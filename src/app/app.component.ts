2b38fab0933d19e14ce769dd717f754118d2aa06
Michael Short <michael@Michael.local>
2022-12-27 22:34:58 +1030

    finished for the night, completed the win condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText = lorem.sentence();
+  enteredText = ''
+  win = this.randomText == this.enteredText;
+
+  onInputChange(event) {
+    this.enteredText = event;
+    console.log(this.win)
+  }
 }
